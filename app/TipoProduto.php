5036d3043a1f4fb9206f0363f30ba925aa1b86d6
Alanmoreira01 <ciceroalan2003@gmail.com>
2020-06-29 07:18:14 -0300

    cofiguração do nodel TipoProduto

--- app/TipoProduto.php
+++ app/TipoProduto.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class TipoProduto extends Model
 {
-    //
+    protected $table = 'tipo_produto';
+    protected $primaryKey = 'id_tipo_produto';
+    protected $guarded = ['id_tipo_produto'];
+    static $rules = [
+        'ds_tipo_produto' => 'required|min:5|max:255',
+
+    ];
+
+
+    public function categoria() {
+        return $this->hasMany('App\Models\Categoria', 'id_categoria','id_tipo_produto');
+    }
 }
